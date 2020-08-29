d29520edf612944a4cbfcad61b8902c89dabc0a2
Termux-br <68141664+T3rMuX0@users.noreply.github.com>
2020-08-29 00:19:24 -0300

    Update consultav3.py

--- consultav3.py
+++ consultav3.py
@@ -11,8 +11,8 @@ ng='\033[1m'
 f='\033[m'
 lz='\033[34m'   
 def ip():  
-    n1 = int(input(f'{vm}--={f}{az}[CONSULTA IP]{f}\n\n- {az}[{f}{lz}01{f}{az}]{f} {ng}ipgeolocation{f}\n- {az}[{f}{lz}02{f}{az}]{f} {ng}ip{f}\n- {az}[{f}{lz}03{f}{az}]{f} {ng}ipfind{f}  \n- {am}+=>{f} '))         
-    if n1 == 1:
+    n1 = str(input(f'{vm}--={f}{az}[CONSULTA IP]{f}\n\n- {az}[{f}{lz}01{f}{az}]{f} {ng}ipgeolocation{f}\n- {az}[{f}{lz}02{f}{az}]{f} {ng}ip{f}\n- {az}[{f}{lz}03{f}{az}]{f} {ng}ipfind{f}  \n- {am}+=>{f} '))         
+    if n1=='1' or n1=='01':
         ip1=input(f'{vm}--=={f}{az}[fonte IPGEOLOCATION]{f} \n{vm}--=={f}{az}[ ip ]{f} {am}+=>{f} ')
         url1 = "https://api.ipgeolocation.io/ipgeo?apiKey=9313e7887bad45cea6d4b5845f085464&ip={}".format(ip1)
         res = requests.get(url1)
@@ -21,22 +21,22 @@ def ip():
         print(br)
 
 
-    elif n1 == 2:            
+    elif n1=='2' or n1=='02':            
         ip2=input(f'{vm}--=={f}{az}[fonte IP ]{f} \n{vm}--=={f}{az}[ ip ]{f} {am}+=>{f} ')      
         url2='http://ip-api.com/json/{}'.format(ip2)
         res2=requests.get(url2);req1=res2.json()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         br2="\n{}[+] CONSULTA REALIZADA PELO SCRIPT DE CONSULTA-V3 \n[+] by: ∆ristoteles{}\n {}-=[status]:{}\n -=[país]:{}\n -=[código do país]:{}\n -=[região]:{}\n -=[nome da região]:{}\n -=[cidade]:{}\n -=[zip]:{}\n -=[lat]:{}\n -=[lon]:{}\n -=[fuso horário]:{}\n -=[isp]:{}\n -=[org]:{}\n -=[as]:{} {}\033[m".format(ng,f,vd,req1['status'],req1['country'],req1['countryCode'],req1['region'],req1['regionName'],req1['city'],req1['zip'],req1['lat'],req1['lon'],req1['timezone'],req1['isp'],req1['org'],req1['as'],req1['query'],f)       
         print(br2)
         exit()
-    elif n1 == 3:     
+    elif n1=='3' or n1=='03':     
         ip3=input(f'{vm}--=={f}{az}[fonte IPFIND ]{f} \n{vm}--=={f}{az}[ ip ]{f} {am}+=>{f} ')
         url3='https://api.ipfind.com/?ip={}&auth=22e75f18-7853-4227-ac49-3a8a72893210'.format(ip3)
         res3=requests.get(url3);req3=res3.json()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         br3='\n{}[+] CONSULTA REALIZADA PELO SCRIPT DE CONSULTA-V3 \n[+] by: ∆ristoteles{}\n{}-=[ip_address]:{}\n-=[country]:{}\n-=[country code]:{}\n-=[continent]:{}\n-=[continente codigo]:{}\n-=[city]:{}\n-=[condado]:{}\n-=[região]:{}\n-=[código da região]:{}\n-=[fuso horário]:{}\n-=[proprietário]:{}\n-=[longitude]:{}\n-=[latitude]:{}\n-=[moeda]:{}\n-=[línguas]:{}\n{}'.format(ng,f,vd,req3['ip_address'],req3['country'],req3['country_code'],req3['continent'],req3['continent_code'],req3['city'],req3['county'],req3['region'],req3['region_code'],req3['timezone'],req3['owner'],req3['longitude'],req3['latitude'],req3['currency'],req3['languages'],f)
         print(br3)
 def cep():    
-    n1 = int(input(f'{vm}--={f}{az}[CONSULTA CEP]{f}\n\n- {az}[{f}{lz}1{f}{az}]{f} {ng}postmon{f}\n- {az}[{f}{lz}2{f}{az}]{f} {ng}viacep{f}\n- {az}[{f}{lz}3{f}{az}]{f} {ng}apicep{f}  \n- {am}+=>{f} '))
-    if n1==1:
+    n1 = str(input(f'{vm}--={f}{az}[CONSULTA CEP]{f}\n\n- {az}[{f}{lz}1{f}{az}]{f} {ng}postmon{f}\n- {az}[{f}{lz}2{f}{az}]{f} {ng}viacep{f}\n- {az}[{f}{lz}3{f}{az}]{f} {ng}apicep{f}  \n- {am}+=>{f} '))
+    if n1=='1' or n1=='01':
         cep1=input(f'{vm}--=={f}{az}[fonte POSTMON ]{f} \n{vm}--=={f}{az}[ cep ]{f} {am}+=>{f} ')
         if len(cep1) == 8:
             url1='https://api.postmon.com.br/v1/cep/{}'.format(cep1)
@@ -45,7 +45,7 @@ def cep():
             print(resl1)
         else:
             print(f' {vm}[!] valor invalido \n digite apenas numeros \n {f}ex: {ng}01001000 {f}')                                 
-    elif n1==2:
+    elif n1=='2' or n1=='02':
         cep2=input(f'{vm}--=={f}{az}[fonte VIACEP ]{f} \n{vm}--=={f}{az}[ cep ]{f} {am}+=>{f} ')
         if len(cep2)==8:
             url2='https://viacep.com.br/ws/{}/json/'.format(cep2)
@@ -54,7 +54,7 @@ def cep():
             print(resl2)
         else:
             print(f' {vm}[!] valor invalido \n digite apenas numeros \n {f}ex: {ng}01001000 {f}')
-    elif n1==3:
+    elif n1=='3' or n1=='03':
         cep3=input(f'{vm}--=={f}{az}[fonte APICEP ]{f} \n{vm}--=={f}{az}[ cep ]{f} {am}+=>{f} ')
         if len(cep3) == 8:            
             url3='https://ws.apicep.com/cep/{}.json'.format(cep3) 
@@ -103,9 +103,9 @@ try:
         os.system('clear');ban();ip()
     elif inpu=='2' or inpu=='02':
         os.system('clear');ban();cep()
-    elif inpu=='3\ or inpu=='03':
+    elif inpu=='3' or inpu=='03':
         os.system('clear');ban();cnpj()
-    elif inpu=='4' or inpu=='03':
+    elif inpu=='4' or inpu=='04':
         exit()
     else:
         print(f'{vm}- [!] valor invalido !! {f}');exit()                                                                                          
